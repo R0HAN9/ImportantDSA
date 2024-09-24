1b3cb07daec2b91a9315ce903d88dd6d608f74fc
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-09-24 09:57:57 +0530

    Update 02.java

--- 02.java
+++ 02.java
@@ -58,3 +58,26 @@ class Solution {
         return res;        
     }
 }
+
+
+// 3. Container With Most Water
+
+class Solution {
+    public int maxArea(int[] height) {
+        int maxArea = 0;
+        int left = 0;
+        int right = height.length - 1;
+
+        while (left < right) {
+            maxArea = Math.max(maxArea, (right - left) * Math.min(height[left], height[right]));
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;        
+    }
+}
