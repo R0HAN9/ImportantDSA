9d43516189f59b1894d2e2f81853da6dd880b682
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-09-24 10:19:28 +0530

    Update 02.java

--- 02.java
+++ 02.java
@@ -103,4 +103,24 @@ class Solution {
 }
 
 
+// 5. Best Time to Buy and Sell Stock
+
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buyPrice = prices[0];
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (buyPrice > prices[i]) {
+                buyPrice = prices[i];
+            }
+
+            profit = Math.max(profit, prices[i] - buyPrice);
+        }
+
+        return profit;        
+    }
+}
+
 
