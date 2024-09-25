7f49a40eaf1b1c783bfb8b4d6a39014ce2d91412
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-09-25 10:57:34 +0530

    Update 03.java

--- 03.java
+++ 03.java
@@ -122,7 +122,24 @@ class Solution {
 }
 
 
+// 5. Invert Binary Tree
 
 
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        
+        invertTree(root.left);
+        invertTree(root.right);
+        
+        return root;        
+    }
+}
 
 
