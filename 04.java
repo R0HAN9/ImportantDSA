2350d8d5cc98e84050c1112385c4ed47d31a832c
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-09-26 10:50:50 +0530

    Update 04.java

--- 04.java
+++ 04.java
@@ -76,3 +76,25 @@ class Solution {
         return slow;        
     }
 }
+
+
+// 4. Valid Anagram
+
+
+ class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> sCount = new HashMap<>();
+        HashMap<Character, Integer> tCount = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            sCount.put(s.charAt(i), 1 + sCount.getOrDefault(s.charAt(i), 0));
+            tCount.put(t.charAt(i), 1 + tCount.getOrDefault(t.charAt(i), 0));
+        }
+
+        return sCount.equals(tCount);        
+    }
+}
