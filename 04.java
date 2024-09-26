2c041dd6636f7647e05eca7b7839f37140f579e8
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-09-26 11:00:46 +0530

    Update 04.java

--- 04.java
+++ 04.java
@@ -98,3 +98,26 @@ class Solution {
         return sCount.equals(tCount);        
     }
 }
+
+
+// 5. Binary Search
+
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int l=0;
+        int h=nums.length-1;
+        while(l<=h){
+            int mid = (l+h)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[mid]>target){
+                h = mid-1;
+            }else{
+                l = mid+1;
+            }
+        }
+        return -1;
+    }
+}
