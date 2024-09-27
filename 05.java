fb27f6e83799ed2ac4b70efc0da9250283198ce8
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-09-27 11:10:10 +0530

    Update 05.java

--- 05.java
+++ 05.java
@@ -77,3 +77,25 @@ class Solution {
         return dummy.next;        
     }
 }
+
+
+// 4. Linked List Cycle
+
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
