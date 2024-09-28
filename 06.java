b286f2e7b44a5c5f9e205d8ba5bc122b0464fe0d
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-09-28 11:39:39 +0530

    Update 06.java

--- 06.java
+++ 06.java
@@ -77,4 +77,18 @@ class Solution {
 }
 
 
+// 4. Single Number
 
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+        int n = nums.length;
+        int c = 0;
+
+        for (int i = 0; i < n; i++) {
+            c = c ^ nums[i];
+        }
+        return c;
+    }
+}
