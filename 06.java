106e0df6e4af3f6796d00281e6039af945714e3e
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-09-28 11:01:29 +0530

    Update 06.java

--- 06.java
+++ 06.java
@@ -28,3 +28,23 @@ class Solution {
     }
 }
 
+
+// 2. Daily Temperatures
+
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
+        int[] result = new int[temperatures.length];
+
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int idx = stack.pop();
+                result[idx] = i - idx;
+            }
+            stack.push(i);
+        }
+
+        return result;        
+    }
+}
