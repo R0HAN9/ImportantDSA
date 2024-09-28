497654506f55e6fcf24d21814d251f3f99f62040
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-09-28 11:40:19 +0530

    Update 06.java

--- 06.java
+++ 06.java
@@ -92,3 +92,20 @@ class Solution {
         return c;
     }
 }
+
+
+5. class Solution {
+    public ListNode reverseList(ListNode head) {
+        
+        ListNode node = null;
+
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = node;
+            node = head;
+            head = temp;
+        }
+
+        return node;
+    }
+}
