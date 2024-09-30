674fb350203bbfcf4ce3cade7c338514d3b90a44
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-09-30 11:05:21 +0530

    Update 07.java

--- 07.java
+++ 07.java
@@ -2,48 +2,67 @@
 
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        k = nums.length - k;
-
-        Random random = new Random();
-        for (int i = nums.length - 1; i >= 0; i--) {
-            int j = random.nextInt(i + 1);
-            int temp = nums[i];
-
-            nums[i] = nums[j];
-            nums[j] = temp;
-        } 
-        return quickSelect(nums, 0, nums.length - 1, k);
-    }
-
-    private int quickSelect(int[] nums, int left, int right, int k) {
-        int pivot = nums[right];
-        int p = left;
-
-        for (int i = left; i < right; i++) {
-            if (nums[i] <= pivot) {
-                int temp = nums[i];
-
-                nums[i] = nums[p];
-                nums[p] = temp;
-                p++;
+        
+        int main_max = Integer.MIN_VALUE;
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        
+        for(int i=0;i<nums.length;i++){
+            if(hm.containsKey(nums[i])){
+                hm.put(nums[i],hm.get(nums[i]) +1);
+            } else{
+                hm.put(nums[i],1);
+            }
+            main_max = Math.max(main_max,nums[i]);
+        }
+        
+        if(hm.size()==1){
+            return main_max;
+        }
+        
+        k -= hm.get(main_max);
+        while(k>0){
+            main_max--;
+            if(hm.containsKey(main_max)){
+                k -= hm.get(main_max);
             }
         }
-
-        int temp = nums[p];
-        nums[p] = nums[right];
-        nums[right] = temp;
-
-        if (p > k) {
-            return quickSelect(nums, left, p - 1, k);
-        } else if (p < k) {
-            return quickSelect(nums, p + 1, right, k);
-        } else {
-            return nums[p];
-        }
+        return main_max;
     }
 }
 
 //2. Top K Frequent Elements
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> counter = new HashMap<>();
+        for (int n : nums) {
+            counter.put(n, counter.getOrDefault(n, 0) + 1);
+        }
+        
+        List<Integer>[] freq = new ArrayList[nums.length + 1];
+        for (int i = 0; i < freq.length; i++) {
+            freq[i] = new ArrayList<>();
+        }
+        
+        for (Map.Entry<Integer, Integer> entry : counter.entrySet()) {
+            int frequency = entry.getValue();
+            freq[frequency].add(entry.getKey());
+        }
+        
+        int[] res = new int[k];
+        int idx = 0;
+        for (int i = freq.length - 1; i >= 0; i--) {
+            for (int num : freq[i]) {
+                res[idx++] = num;
+                if (idx == k) {
+                    return res;
+                }
+            }
+        }
+        
+        return new int[0];        
+    }
+}
 
 //3. Find K Pairs with Smallest Sums
 
