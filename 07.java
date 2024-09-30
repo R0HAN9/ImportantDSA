bd8e3920a7cd789d1f45f40e81d77bfdb4b36d8e
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-09-30 11:40:35 +0530

    Update 07.java

--- 07.java
+++ 07.java
@@ -110,4 +110,20 @@ class Solution {
 
 //4. Majority Element
 
+class Solution {
+    public int majorityElement(int[] nums) {
+        
+        int res = 0;
+        int majority = 0;
+
+        for (int n : nums) {
+            if (majority == 0) {
+                res = n;
+            }
+            majority += n == res ? 1 : -1;
+        }
+        return res;
+    }
+}
+
 //5. Missing Number
