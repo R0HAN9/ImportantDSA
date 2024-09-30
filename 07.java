1e4b196dbe742db548c9f648bd83a6d934382e33
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-09-30 11:56:11 +0530

    Update 07.java

--- 07.java
+++ 07.java
@@ -127,3 +127,15 @@ class Solution {
 }
 
 //5. Missing Number
+
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        
+        int result = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            result += i - nums[i];
+        }
+        return result;
+    }
+}
