e4f1e63f7912f28f836e7384a21cd72e89df7d7c
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-01 13:28:51 +0530

    Update 08.java

--- 08.java
+++ 08.java
@@ -57,6 +57,25 @@ class Solution {
 
 // 3. Non-Overlapping Intervals
 
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        int res = 0;
+
+        Arrays.sort(intervals, (a, b) -> a[1] - b[1]);
+        int prev_end = intervals[0][1];
+
+        for (int i = 1; i < intervals.length; i++) {
+            if (prev_end > intervals[i][0]) {
+                res++;
+            } else {
+                prev_end = intervals[i][1];
+            }
+        }
+
+        return res;        
+    }
+}
+
 // 4. Reverse String
 
 // 5. Diameter of Binary Tree
