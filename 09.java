b9e2057e0630fd2aaa314ebacb02e40c6ce838f4
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-02 10:52:38 +0530

    Update 09.java

--- 09.java
+++ 09.java
@@ -31,6 +31,29 @@ class Solution {
 
 // 2. Find Minimum in Rotated Sorted Array
 
+class Solution {
+    public int findMin(int[] nums) {
+        
+        int len = nums.length;
+        int startIdx = 0;
+        int endIdx = len - 1;
+        int ans = Integer.MAX_VALUE;
+
+        while (startIdx <= endIdx) {
+            int mid = startIdx + (endIdx - startIdx) / 2;
+
+            if (nums[mid] > nums[endIdx]) {
+                startIdx = mid + 1;
+            }
+            else if (nums[mid] <= nums[endIdx]) {
+                ans = Math.min(ans, nums[mid]);
+                endIdx = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
+
 // 3. Search a 2D Matrix II
 
 // 4. Middle of the Linked List
