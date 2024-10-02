717d130fea55d25eb3b1b0443a3c66f123f73b6c
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-02 10:37:02 +0530

    Update 09.java

--- 09.java
+++ 09.java
@@ -1,5 +1,34 @@
 // 1. Search in Rotated Sorted Array 
 
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] >= nums[left]) {
+                if (nums[left] <= target && target <= nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] <= target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;        
+    }
+}
+
 // 2. Find Minimum in Rotated Sorted Array
 
 // 3. Search a 2D Matrix II
