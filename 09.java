46b89aa49c8b853349ae38a0297dcdfa4de2641c
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-02 11:23:06 +0530

    Update 09.java

--- 09.java
+++ 09.java
@@ -75,4 +75,25 @@ class Solution {
 
 // 4. Middle of the Linked List
 
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        
+        if (head == null || head.next == null) 
+            return head;
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null) {
+            fast = fast.next;
+
+            if (fast != null) {
+                fast = fast.next;
+                slow = slow.next;
+            }
+        }
+        return slow;
+    }
+}
+
 // 5. Convert Sorted Array to Binary Search Tree
