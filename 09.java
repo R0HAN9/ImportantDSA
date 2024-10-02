032eca6435c996064b9abbb0c29ab2ded7ea0c4e
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-02 11:13:11 +0530

    Update 09.java

--- 09.java
+++ 09.java
@@ -56,6 +56,23 @@ class Solution {
 
 // 3. Search a 2D Matrix II
 
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int row = 0, col = m - 1;
+
+        while (row < n && col >= 0) {
+            if (matrix[row][col] == target) return true;
+
+            else if (matrix[row][col] < target) row++;
+            else col--;
+        }
+        return false;
+    }
+}
+
 // 4. Middle of the Linked List
 
 // 5. Convert Sorted Array to Binary Search Tree
