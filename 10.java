ee945fab20103f93f1d645c22caeda36a2340679
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-03 10:22:36 +0530

    Update 10.java

--- 10.java
+++ 10.java
@@ -31,4 +31,29 @@ class Solution {
 
 // 2. InOrder → Kth Smallest Element in a BST 
 
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        
+        int[] count = new int[]{0};
+        int[] kthSmallest = new int[] {Integer.MIN_VALUE};
+
+        inOrderHelper(root, count, k, kthSmallest);
+        return kthSmallest[0];
+
+    }
+    public void inOrderHelper(TreeNode root, int[] count, int k, int[] kthSmallest) {
+        if (root == null || count[0] >= k) return;
+
+        inOrderHelper(root.left, count, k, kthSmallest);
+        count[0]++;
+
+        if (count[0] == k) {
+            kthSmallest[0] = root.val;
+            return;
+        } 
+
+        inOrderHelper(root.right, count, k, kthSmallest);
+    }
+}
+
 // 3. PostOrder → Binary Tree Maximum Path Sum 
