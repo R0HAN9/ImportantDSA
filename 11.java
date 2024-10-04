9df9d59d336f05d1178f5ad8f67007a1d9569d6b
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-04 10:49:43 +0530

    Update 11.java

--- 11.java
+++ 11.java
@@ -32,4 +32,28 @@ class Solution {
 
 // 2. Path Sum II
 
+class Solution {
+
+    List<List<Integer>> answer = new ArrayList<List<Integer>>();
+
+    private void dfs(TreeNode node, List<Integer> path, int remainingSum) {
+
+        if (node == null) return;
+        path.add(node.val);
+
+        if (node.left == null && node.right == null && remainingSum == node.val) answer.add(new ArrayList<>(path));
+        this.dfs(node.left, path, remainingSum - node.val);
+        this.dfs(node.right, path, remainingSum - node.val);
+
+        path.remove(path.size() - 1);
+    }
+
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        
+        List<Integer> path = new ArrayList<Integer>();
+        dfs(root, path, targetSum);
+        return answer;
+    }
+}
+
 // 3. Course Schedule II
