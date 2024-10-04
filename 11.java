d4befe3eaccb134bd1c3aa165bc10b684caa01ae
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-04 11:30:55 +0530

    Update 11.java

--- 11.java
+++ 11.java
@@ -130,3 +130,27 @@ class Solution {
 }
 
 // 5. Product of Array Except Self
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        
+        int[] output = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            output[i] = 1;
+        }
+
+        int left = 1;
+        for (int i = 0; i < nums.length; i++) {
+            output[i] *= left;
+            left *= nums[i];
+        }
+
+        int right = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            output[i] *= right;
+            right *= nums[i];
+        }
+
+        return output;
+    }
+}
