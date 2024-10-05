4cbc4be18d4a5dcee14c9bacff9880e3ce452233
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-05 10:47:12 +0530

    Update 12.java

--- 12.java
+++ 12.java
@@ -91,3 +91,57 @@ class Solution {
 }
 
 // 3. Word Ladder
+
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+
+        Set<String> wordSet = new HashSet<>();
+        Boolean isPresent = false;
+
+        wordSet.addAll(wordList);
+        for (String currWord : wordList) {
+            if (endWord.equals(currWord)) {
+                isPresent = true;
+                break;
+            }
+        }
+
+        if (!isPresent)
+            return 0;
+        Queue<String> wordQueue = new LinkedList<>();
+
+        wordQueue.add(beginWord);
+        int distance = 0;
+
+        while (!wordQueue.isEmpty()) {
+            int size = wordQueue.size();
+            distance++;
+
+            while (size-- != 0) {
+                String currWord = wordQueue.poll();
+
+                for (int i = 0; i < currWord.length(); i++) {
+                    char[] temp = currWord.toCharArray();
+
+                    for (char j = 'a'; j <= 'z'; j++) {
+                        temp[i] = j;
+
+                        String newWord = new String(temp);
+
+                        if (newWord.equals(endWord))
+                            return distance + 1;
+                        if (wordSet.contains(newWord)) {
+                            wordQueue.add(newWord);
+                            wordSet.remove(newWord);
+
+                            System.out.println(newWord);
+                        }
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+}
+
+
