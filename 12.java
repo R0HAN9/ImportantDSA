0ca9035dde7acecaae21d01de7366fba5b168cb8
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-05 10:24:10 +0530

    Update 12.java

--- 12.java
+++ 12.java
@@ -33,4 +33,61 @@ class Solution {
 
 // 2. Rotting Oranges 
 
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] visited = grid;
+
+        Queue<int[]> queue = new LinkedList<>();
+        int countFreshOrange = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (visited[i][j] == 2) {
+                    queue.offer(new int[] {i, j});
+                }
+                if (visited[i][j] == 1) {
+                    countFreshOrange++;
+                }
+            }
+        }
+        if (countFreshOrange == 0) return 0;
+        if (queue.isEmpty()) return -1;
+
+        int minutes = -1;
+        int[][] dirs = {{1,0}, {-1,0}, {0,-1}, {0,1}};
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            while (size-- > 0) {
+                int[] cell = queue.poll();
+
+                int x = cell[0];
+                int y = cell[1];
+
+                for (int[] dir : dirs) {
+                    int i = x + dir[0];
+                    int j = y + dir[1];
+
+                    if (i >= 0 && i < m && j >= 0 && j <n && visited[i][j] == 1) {
+                        visited[i][j] = 2;
+                        countFreshOrange--;
+                        queue.offer(new int[] {i, j});
+                    }
+                }
+            }
+            minutes++;
+        }
+
+        if (countFreshOrange == 0) {
+            return minutes;
+        }
+
+        return -1;
+    }
+}
+
 // 3. Word Ladder
