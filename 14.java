9cfc0109d6162499e51b7716e45375c1ea7ab3cc
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-07 11:56:14 +0530

    Update 14.java

--- 14.java
+++ 14.java
@@ -175,3 +175,17 @@ class MinStack {
 }
 
 // 5. Contains Duplicate 
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        Arrays.sort(nums);
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
