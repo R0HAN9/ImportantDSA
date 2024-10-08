f9a0b2a96d6cfe2604ce391a66875110d9c570a5
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-10-08 11:25:46 +0530

    Update 15.java

--- 15.java
+++ 15.java
@@ -101,3 +101,36 @@ class Solution {
 }
 
 // 5. Partition Equal Subset Sum 
+
+class Solution {
+    public boolean canPartition(int[] nums) {
+        
+        int sum = 0;
+        for (int elements : nums) {
+            sum += elements;
+        }
+
+        if (sum % 2 != 0) return false;
+        sum = sum / 2;
+
+        boolean dp[] = new boolean[sum + 1];
+        dp[0] = true;
+
+        for (int num : nums) {
+            for (int i = sum; i > 0; i--) {
+
+                if (i >= num) {
+                    dp[i] = dp[i] || dp[i - num];
+                }
+            }
+        }
+        return dp[sum];
+    }
+}
+
+
+// 6. Kth Smallest Element in a BST
+
+
+
+// 7. Merge Intervals 
