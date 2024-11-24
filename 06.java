9878fac676f9544eda13b832fbf6338de8d0e13b
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-11-24 08:32:20 +0530

    Update 06.java

--- 06.java
+++ 06.java
@@ -3,29 +3,44 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         
+        // Stack to keep track of the elements in nums2 in decreasing order
         Stack<Integer> s1 = new Stack<>();
+        
+        // Map to store the next greater element for each number in nums2
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        for (int i = nums2.length-1; i >= 0; i--) {
+        // Traverse nums2 in reverse order to find the next greater element for each number
+        for (int i = nums2.length - 1; i >= 0; i--) {
             int num = nums2[i];
 
+            // Pop all elements from the stack which are less than or equal to the current number
             while (!s1.isEmpty() && s1.peek() <= num) {
                 s1.pop();
             }
 
-            if(s1.isEmpty()) map.put(num, -1);
-            else map.put(num, s1.peek());
+            // If the stack is empty, no greater element exists, so we map it to -1
+            if (s1.isEmpty()) {
+                map.put(num, -1);
+            } else {
+                // If the stack is not empty, the top element of the stack is the next greater element
+                map.put(num, s1.peek());
+            }
 
+            // Push the current number onto the stack to be used for future comparisons
             s1.push(num);
         }
 
+        // Now, for each element in nums1, find its next greater element from the map
         for (int i = 0; i < nums1.length; i++) {
+            // If the element exists in the map, set it to the next greater element; otherwise, set it to -1
             nums1[i] = map.containsKey(nums1[i]) ? map.get(nums1[i]) : -1;
         }
 
+        // Return the result array with the next greater elements
         return nums1;
     }
 }
+
 
 
 
