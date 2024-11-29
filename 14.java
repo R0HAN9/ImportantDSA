504127129e4f691ffeaebd949df8c4840577cb9d
ROHAN GUPTA <95059582+R0HAN9@users.noreply.github.com>
2024-11-29 09:24:24 +0530

    Update 14.java

--- 14.java
+++ 14.java
@@ -3,38 +3,58 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         
+        // Create a list to store all permutations of the input array
         List<List<Integer>> result = new ArrayList<>();
+
+        // Base case: if the array has only one element, return a list containing that element as a single permutation
         if (nums.length == 1) {
 
+            // Create a single-element list and add the element from the input array
             List<Integer> singleList = new ArrayList<>();
             singleList.add(nums[0]);
-            result.add(singleList);
-            return result;
 
+            // Add the single-element list to the result list
+            result.add(singleList);
+
+            // Return the result containing one permutation
+            return result;
         }
+
+        // Iterate over each element in the array to consider it as the first element in a permutation
         for (int i = 0; i < nums.length; i++) {
 
+            // Pick the current element to be fixed in the current permutation
             int n = nums[i];
+
+            // Create a new array to hold all elements except the current element
             int[] remainingNums = new int[nums.length - 1];
             int index = 0;
 
+            // Populate the remainingNums array with all elements except the one at index 'i'
             for (int j = 0; j < nums.length; j++) {
                 if (j != i) {
                     remainingNums[index] = nums[j];
                     index++;
                 }
             }
+
+            // Recursively find all permutations of the remaining elements
             List<List<Integer>> perms = permute(remainingNums);
+
+            // For each permutation of the remaining elements, add the current fixed element 'n' to it
             for (List<Integer> p : perms) {
                 p.add(n);
             }
 
+            // Add all permutations created in this iteration to the final result
             result.addAll(perms);
         }
 
+        // Return the complete list of permutations
         return result;
     }
 }
+
 
 // 2. Subsets 
 
